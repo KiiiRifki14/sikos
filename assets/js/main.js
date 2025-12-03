28c4ead20d1b297d2884090f9afe119645652829
KiiiRifki_14 <muhammadrifki08t@gmail.com>
2025-12-03 21:34:20 +0700

    3.1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
-
 function toggleSidebar() {
     document.querySelector('.sidebar').classList.toggle('active');
 }// assets/js/main.js
 document.addEventListener('DOMContentLoaded', function(){
     console.log('SIKOS App Loaded');
-
+    
     // Contoh animasi sederhana atau validasi form bisa ditaruh di sini
 });
